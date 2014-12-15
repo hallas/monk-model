36731d8437c6be18a77b87f7962d923324188869
Christoffer Hallas <christoffer.hallas@gmail.com>
2014-12-15 07:46:00 +0100

    bumped, removed mongodb functionality, now purely about modeling

--- test/lib/mongel.test.js
+++ test/lib/mongel.test.js
@@ -1,237 +1,26 @@
-var model   = require('../..');
-var assert  = require('assert');
-var Session = require('prosession');
+var model = require('../..');
+var assert = require('assert');
 
-var session = new Session('mongodb://localhost/mongel-test');
+var Car = model();
 
 describe('lib mongel', function () {
 
-  describe('model', function () {
+  it('should instantiate', function () {
+    var car = new Car({ color: 'green' });
+    assert.equal(car.color, 'green');
+  });
 
-    var Car;
+  it('should instantiate array', function () {
+    var cars = new Car([ { color: 'yellow' }, { color: 'red' } ]);
+    assert.equal(cars.length, 2);
 
-    before(function (done) {
-      //Car = model('items', 'mongodb://localhost/mongel-test');
-      Car = model('items', session);
-      Car.session.on('connected', done);
-    });
+    var car = cars.pop();
+    assert.equal(car.color, 'red');
+  });
 
-    before(function* () {
-      yield Car.dropIndexes();
-      yield Car.remove();
-    });
-
-    it('should create', function () {
-      assert(Car);
-    });
-
-    it('should execute custom command', function* () {
-      var cars = yield Car.session.command(function (db, done) {
-        Car.collection.insert({ color: 'red', engine: 'V8' }, function (err, doc) {
-          if (err) return done(err);
-          done(null, doc);
-        });
-      });
-      assert(Array.isArray(cars));
-
-      var car = cars.shift();
-      assert(car._id);
-      assert.equal(car.color, 'red');
-      assert.equal(car.engine, 'V8');
-    });
-
-    describe('index', function () {
-
-      it('should index', function* () {
-        var index = yield Car.ensureIndex('color');
-        assert.equal(index, 'color_1');
-      });
-
-      it('should get index information', function* () {
-        var information = yield Car.indexInformation();
-        var colorIndex = information.color_1[0];
-        assert(colorIndex);
-        assert.equal(colorIndex[0], 'color');
-        assert.equal(colorIndex[1], 1);
-      });
-
-    });
-
-    describe('create', function () {
-
-      it('should create', function* () {
-        var cars = yield Car.create({ color: 'yellow', engine: 'V4' });
-        assert(Array.isArray(cars));
-        var car = cars[0];
-        assert(car);
-        assert(car instanceof Car);
-        assert(car._id);
-        assert.equal(car.color, 'yellow');
-        assert.equal(car.engine, 'V4');
-      });
-
-      it('should create one', function* () {
-        var car = yield Car.createOne({ color: 'yellow', engine: 'V4' });
-        assert(car);
-        assert(car instanceof Car);
-        assert(car._id);
-        assert.equal(car.color, 'yellow');
-        assert.equal(car.engine, 'V4');
-      });
-
-    });
-
-    describe('remove', function () {
-
-      var car;
-
-      beforeEach(function* () {
-        yield Car.remove();
-        car = yield Car.createOne({ color: 'green', engine: 'V2' });
-      });
-
-      it('should remove', function* () {
-        var result = yield car.remove();
-        assert(result.ok);
-        assert.equal(result.n, 1);
-        car = yield car.fetch();
-        assert(!car);
-      });
-
-      it('should remove statically', function* () {
-        var result = yield Car.remove();
-        assert(result.ok);
-        assert(result.n > 0);
-        car = yield car.fetch();
-        assert(!car);
-      });
-
-      it('should remove by id', function* () {
-        var result = yield Car.removeById(car._id);
-        assert(result.ok);
-        assert.equal(result.n, 1);
-        car = yield car.fetch();
-        assert(!car);
-      });
-
-    });
-
-    describe('update', function () {
-
-      var car;
-
-      beforeEach(function* () {
-        yield Car.remove();
-        car = yield Car.createOne({ color: 'green', engine: 'V2' });
-      });
-
-      it('should update', function* () {
-        assert.equal(car.color, 'green');
-        car = yield car.update({ $set: { color: 'purple' } });
-        assert.equal(car.color, 'purple');
-      });
-
-      it('should update by id', function* () {
-        assert.equal(car.color, 'green');
-        var result = yield Car.updateById(car._id, { $set: { color: 'purple' } });
-        assert(result.ok);
-        assert(result.n, 1);
-        car = yield car.fetch();
-        assert.equal(car.color, 'purple');
-      });
-
-      it('should update statically', function* () {
-        assert.equal(car.color, 'green');
-        var result = yield Car.update({ _id: car._id }, { $set: { color: 'purple' } });
-        assert(result.ok);
-        assert(result.n, 1);
-        car = yield car.fetch();
-        assert.equal(car.color, 'purple');
-      });
-
-    });
-
-    describe('find', function () {
-
-      var car;
-
-      before(function* () {
-        yield Car.remove();
-        car = yield Car.createOne({ color: 'blue', engine: 'V6' });
-      });
-
-      it('should fetch', function* () {
-        car = yield car.fetch();
-        assert(car);
-        assert(car instanceof Car);
-        assert.equal(car.color, 'blue');
-        assert.equal(car.engine, 'V6');
-      });
-
-      it('should find', function* () {
-        var cars = yield Car.find();
-        assert(Array.isArray(cars));
-        var car = cars[0];
-        assert(car instanceof Car);
-        assert.equal(car.color, 'blue');
-        assert.equal(car.engine, 'V6');
-      });
-
-      it('should find with cursor', function* () {
-        var cursor = yield Car.findCursor();
-        assert(cursor);
-
-        var doc = yield function (done) {
-          cursor.nextObject(done);
-        };
-        assert(doc);
-        assert.equal(doc.color, 'blue');
-        assert.equal(doc.engine, 'V6');
-      });
-
-      describe('findOne', function () {
-
-        it('should find one by color', function* () {
-          var car = yield Car.findOne({ color: 'blue' });
-          assert(car instanceof Car);
-          assert.equal(car.color, 'blue');
-          assert.equal(car.engine, 'V6');
-        });
-
-        it('should find one by engine', function* () {
-          var car = yield Car.findOne({ engine: 'V6' });
-          assert(car instanceof Car);
-          assert.equal(car.color, 'blue');
-          assert.equal(car.engine, 'V6');
-        });
-
-      });
-
-      it('should find by id', function* () {
-        var id = car._id;
-        car = null;
-        car = yield Car.findById(id);
-        assert(car instanceof Car);
-        assert.equal(car.color, 'blue');
-        assert.equal(car.engine, 'V6');
-      });
-
-    });
-
-    describe('count', function () {
-
-      beforeEach(function* () {
-        yield Car.remove();
-        yield Car.createOne({ color: 'green', engine: 'V2' });
-      });
-
-      it('should count', function* () {
-        var count = yield Car.count();
-        assert.equal(count, 1);
-      });
-
-    });
-
+  it('should not instantiate', function () {
+    var empty = Car();
+    assert.equal(empty, undefined);
   });
 
 });
